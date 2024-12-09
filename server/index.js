357be195bc7087e051e769dbd4570db56c950961
rajawat-shiv <shiv1999rajawat@gmail.com>
2024-12-09 11:22:26 +0530

    "Updated API URLs from localhost to https://lms-vwcy.onrender.com and updated CORS origin in server/index.js"

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lms-vwcy.onrender.com",
     credentials: true,
   })
 );
